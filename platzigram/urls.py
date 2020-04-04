80f77bba3b37bf054ac010e68d74daf35baf6452
Miguel Torrealba <mtorrealba@techk.cl>
2020-04-04 18:12:55 -0300

    Vista con argumentos y condicional

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -8,5 +8,6 @@ from platzigram import views
 
 urlpatterns = [
     path('hello-world/', views.hello_world),
-    path('hi/', views.hi),
+    path('sorted/', views.sorted_number_list),
+    path('hi/<str:name>/<int:age>/', views.say_hi),
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -15,8 +15,25 @@ def hello_world(request):
     return HttpResponse(f'Oh, hi! Current server time is {now}')
 
 
-def hi(request):
+def sorted_number_list(request):
     """Hi."""
     _numbers = [int(value) for value in request.GET['numbers'].split(',')]
     _numbers.sort()
     return JsonResponse(_numbers, safe=False)
+
+
+def say_hi(request, name, age):
+    if age < 18:
+        message = f'Sorry {name}, you are not allowed here'
+    else:
+        message = f'Hello {name}, Welcome to Platzigram'
+
+    response = {
+        'message': message,
+        'data': {
+            'name': name,
+            'age': age,
+        },
+        'status_code': 200,
+    }
+    return JsonResponse(response)
