35f26dc1e167126cc7479e953428ba43ac98416c
Miguel Torrealba <mtorrealba@techk.cl>
2020-04-10 15:32:38 -0400

    Add login and logout system

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,26 @@
+""" Post admin class"""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+
+    list_display = (
+        'pk',
+        'title',
+        'createdAt',
+    )
+
+    list_editable = (
+    )
+
+    list_display_links = (
+        'pk',
+        'title',
+    )
